4cf4edc149ed3a4389ceba6d0b3b5d7198efc437
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-04-05 21:04:12 +0500

    add model User

--- users/models.py
+++ users/models.py
@@ -1,5 +1,26 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 
 class User(AbstractUser):
-    pass
+    username = None
+
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="Загрузите аватар",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
