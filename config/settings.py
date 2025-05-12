5b17b6b9bd516d802e1e0d0d55aa9fddb2d382fb
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-05-12 20:03:22 +0500

    SECRET_KEY 4

--- config/settings.py
+++ config/settings.py
@@ -9,10 +9,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-if "test" in sys.argv:
-    SECRET_KEY = "secret-key-test"
-else:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
+# if "test" in sys.argv:
+#     SECRET_KEY = "secret-key-test"
+# else:
+#     SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
